56c03d59c2cfde1619408a2af5fc8bd5f5051837
tracey brimage <onelovenilynn@gmail.com>
2019-09-22 02:23:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t (array)
+  if block_given?
    i = 0
 
   while i < array.length
@@ -6,6 +7,7 @@ def hello_t (array)
     i = i + 1
   end
   array
+else
+  put " Hey! No block was given"
 end
-
-h
+end
